225e5e83e8fd0e412a9d9970f0900928a9dc8ca5
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-26 11:52:32 -0400

    Icons
    Started working on the file which would be responsible for holding the icons for each weather pattern

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Temp from "./Temp";
-import CityName from "./CityName"
+import CityName from "./CityName";
+import Icon from "./Icon";
 class App extends React.Component {
 
   render() {
 
-    return <div className = "appHeader"> <Temp/> <CityName/></div>;
+    return <div className = "appHeader"> <Temp/><Icon/> <CityName/></div>;
   }
 }
 export default App;
---
+++ src/Icon.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './Icon.css'
+
+class Icon extends React.Component{
+  state = {
+    Weather:[],
+    Icon:""
+  }
+
+
+
+  getIcon = async() =>{
+    //Put the url in a variable
+    var url =
+      "http://api.openweathermap.org/data/2.5/weather?id=5128638&APPID=faa738d76a8ffaa97bfcdfd989c9eb41";
+
+    //Use fetch to get the information from the url
+    var response = await fetch(url);
+
+    //Use json.() to turn the fetched data into json
+    var json = await response.json();
+
+    //Set the data to temp
+    await this.setState({Weather:json.weather});
+
+    this.setState({Icon: this.state.Weather[0].icon})
+  }
+
+  componentDidMount = () =>{
+    setTimeout(this.getIcon, 200);
+  }
+  render(){
+      var {Weather,Icon} = this.state;
+    return(<div>{Icon}</div>)
+  }
+}
+
+export default Icon;
