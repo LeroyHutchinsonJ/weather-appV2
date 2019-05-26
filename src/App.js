48e521cfce5582bb2a1ebd3dbdad7bc92d4e0285
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-26 11:36:29 -0400

    Created Two new files
    One file is a js file that gets the temp of a city, the other file is a css file to style the js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Temp from "./Temp";
-
+import CityName from "./CityName"
 class App extends React.Component {
 
   render() {
 
-    return <div className = "appHeader"> <Temp/></div>;
+    return <div className = "appHeader"> <Temp/> <CityName/></div>;
   }
 }
 export default App;
---
+++ src/CityName.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./CityName.css"
+
+class CityName extends React.Component{
+  state = {
+    Name: ""
+  }
+  getName = async() =>{
+    //put url in a variable
+    var url = "http://api.openweathermap.org/data/2.5/weather?id=5128638&APPID=faa738d76a8ffaa97bfcdfd989c9eb41";
+
+    //fetch the data from the url
+    var response = await fetch(url);
+
+    //turn the data into a json
+    var json = await response.json();
+
+    //set the data to state in class
+    this.setState({Name:json.name})
+  }
+  componentDidMount = () =>{
+    setTimeout(this.getName, 200);
+  }
+
+  render(){
+    var {Name} = this.state;
+    return(<div>{Name}</div>)
+  }
+}
+
+export default CityName;
