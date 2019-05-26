1fdd36a79f44bbb467c7ec21643ea2b7eef119d1
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-26 11:17:51 -0400

    Finished Temp Variable
    Finished coding the Temp variable

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import './App.css';
+import Temp from "./Temp";
 
 class App extends React.Component {
-  //Something to hold the weather data
-  state = {
-    weatherData: [],
-    weatherName: ""
-  };
-
-  getWeatherData = async() =>{
-    var url =
-      "http://api.openweathermap.org/data/2.5/weather?id=5128638&APPID=faa738d76a8ffaa97bfcdfd989c9eb41";
-
-    var response = await fetch(url);
-
-    var json = await response.json();
-
-    //Adding the json object to the weather data array
-    this.setState({
-        weatherData: json.weather, weatherName: json.name
-    })
-  }
-  componentDidMount = () => {
-
-    setTimeout(this.getWeatherData, 10);
-    console.log(this.state.weatherData);
-  };
 
   render() {
-    var {weatherData,weatherName} = this.state;
-    return <div className = "appHeader">{weatherData.map((weather) => <div key = {1}>{weatherName} {weather.description}</div>)}</div>;
+
+    return <div className = "appHeader"> <Temp/></div>;
   }
 }
 export default App;
--- src/Temp.js
+++ src/Temp.js
@@ -3,8 +3,32 @@ import './Temp.css';
 
 
 class Temp extends React.Component {
+state = {
+Temp: 0
+}
+  getTemp = async() =>{
+    //Put the url in a variable
+    var url =
+      "http://api.openweathermap.org/data/2.5/weather?id=5128638&APPID=faa738d76a8ffaa97bfcdfd989c9eb41";
 
-  render{
-    return(<div></div>)
+    //Use fetch to get the information from the url
+    var response = await fetch(url);
+
+    //Use json.() to turn the fetched data into json
+    var json = await response.json();
+
+    //Set the data to temp
+    this.setState({Temp:json.main.temp});
+  }
+
+  //Set a timer with componentDidMount
+   componentDidMount = () =>{
+     setTimeout(this.getTemp, 200);
+   }
+  render(){
+    var {Temp} = this.state;
+    return(<div>{Temp}</div>)
   }
 }
+
+export default Temp;
